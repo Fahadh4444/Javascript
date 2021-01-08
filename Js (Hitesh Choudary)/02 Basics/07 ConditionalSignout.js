1d540089dbacd816828fa51fae7aca755686be14
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2021-01-08 23:41:18 +0530

    Javascript practiced work concept wise

---
+++ Js (Hitesh Choudary)/02 Basics/07 ConditionalSignout.js
@@ -0,0 +1,17 @@
+// Show user a signout button if he is authenticated,
+// other wise show him option  to Login/Signup
+
+
+var authenticated = true;
+
+// if(authenticated)
+// {
+//     console.log("Show Signout button");
+// }
+// else{
+//     console.log("Show Login button");
+// }
+
+
+
+authenticated ? console.log("Signout Button") : console.log("Login Button");
